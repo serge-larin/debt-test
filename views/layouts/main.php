7fa0318aba9fc72d22e970ce29d5a23709ca8df5
Serge Larin <serge.larin@gmail.com>
2015-12-09 09:19:26 +0300

    Исправлено главное меню

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,16 +36,10 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ?
-                ['label' => 'Login', 'url' => ['/site/login']] :
-                [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ],
+            ['label' => 'Главная', 'url' => ['/site/index']],
+            ['label' => 'Долг', 'url' => ['/debt/default/index']],
+            ['label' => 'О нас', 'url' => ['/site/about']],
+            ['label' => 'Контакты', 'url' => ['/site/contact']],
         ],
     ]);
     NavBar::end();
