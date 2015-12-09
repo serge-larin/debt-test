4a31714bb212573087ed505257a611508077c9e8
Serge Larin <serge.larin@gmail.com>
2015-12-09 13:39:06 +0300

    Добавлена модель DebtSearch

---
+++ models/DebtSearch.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Debt;
+
+/**
+ * DebtSearch represents the model behind the search form about `app\models\Debt`.
+ */
+class DebtSearch extends Debt
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['debt_id', 'debt_person'], 'integer'],
+            [['debt_sum'], 'number'],
+            [['debt_comment', 'debt_created_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Debt::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'debt_id' => $this->debt_id,
+            'debt_sum' => $this->debt_sum,
+            'debt_person' => $this->debt_person,
+            'debt_created_at' => $this->debt_created_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'debt_comment', $this->debt_comment]);
+
+        return $dataProvider;
+    }
+}
